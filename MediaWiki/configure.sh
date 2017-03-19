86f6fbfcc5ad5ef03a430bce30e1d1a0ef143837
Mayank Batra <mayankbatra123@gmail.com>
2017-03-19 17:32:39 +0000

    Additions

--- MediaWiki/configure.sh
+++ MediaWiki/configure.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-mkdir -p /var/www/mediawiki
+#mkdir -p /var/www/mediawiki
 
-ln -s /var/www/mediawiki123 /var/www/mediawiki
+ln -s /var/www/mediawiki123 /var/www/html/mediawiki
 
-chown -R apache:apache /var/www/mediawiki123
-chown -R apache:apache /var/www/mediawiki
+#chown -R apache:apache /var/www/mediawiki123
+#chown -R apache:apache /var/www/mediawiki
 
 #/usr/sbin/httpd -k start -DFOREGROUND
